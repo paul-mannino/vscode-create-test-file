08f90f056a7282c5efc1bb2555663d01a87b43db
Paul Mannino <pmannino.code@gmail.com>
2019-06-24 22:39:15 -0500

    Added configurable settings for name template of test files

--- src/createTest.ts
+++ src/createTest.ts
@@ -2,18 +2,19 @@ import * as vscode from 'vscode';
 import * as path from 'path';
 
 export function createTest(srcUri: vscode.Uri): Thenable<vscode.Uri> {
-    const nameTemplate = '{filename}_spec';
-    let pathMapper = (srcPath: string): string => srcPath.replace('/app', '/spec');
-    let uri = srcUri.with({ path: testPath(srcUri.path, nameTemplate, pathMapper) });
-
-    let we = new vscode.WorkspaceEdit();
-    we.createFile(uri, { overwrite: false });
-    return vscode.workspace.applyEdit(we).then((success) => {
-        if (success) {
-            let relPath = vscode.workspace.asRelativePath(uri);
-            vscode.window.showInformationMessage(`Created ${relPath}`);
-        }
-        return uri;
+    return getExtensionSettings(srcUri).then((settings: ExtensionConfiguration) => {
+        const nameTemplate = settings.get('nameTemplate');
+        let pathMapper = (srcPath: string): string => srcPath.replace('/app', '/spec');
+        let uri = srcUri.with({ path: testPath(srcUri.path, nameTemplate, pathMapper) });
+        let we = new vscode.WorkspaceEdit();
+        we.createFile(uri, { overwrite: false });
+        return vscode.workspace.applyEdit(we).then((success) => {
+            if (success) {
+                let relPath = vscode.workspace.asRelativePath(uri);
+                vscode.window.showInformationMessage(`Created ${relPath}`);
+            }
+            return uri;
+        });
     });
 }
 
@@ -30,4 +31,37 @@ export function testPath(srcPath: string,
     }
     let testBasename = nameTemplate.replace('{filename}', file) + ext;
     return path.posix.join(dir, testBasename);
+}
+
+function getExtensionSettings(srcUri: vscode.Uri): Thenable<ExtensionConfiguration> {
+    return vscode.workspace.openTextDocument(srcUri).then((doc: vscode.TextDocument) => {
+        let docLang = doc.languageId;
+        let langSettings =  vscode.workspace.getConfiguration('createTestFile.languages');
+        let langMatcher = new RegExp(`\\[${docLang}\\]`);
+        let docLangKey = Object.keys(langSettings).find((key: string) => key.match(langMatcher));
+        let langConfig = {};
+        if (docLangKey) {
+            langConfig = langSettings[docLangKey];
+        }
+        let generalConfig = vscode.workspace.getConfiguration('createTestFile');
+        return new ExtensionConfiguration(langConfig, generalConfig);
+    });
+}
+
+class ExtensionConfiguration {
+    languageConfiguration: any;
+    generalConfiguration: vscode.WorkspaceConfiguration;
+
+    constructor(languageConfiguration: any, generalConfiguration: vscode.WorkspaceConfiguration) {
+        this.languageConfiguration = languageConfiguration;
+        this.generalConfiguration = generalConfiguration;
+    }
+
+    get(key: string, defaultValue?: any): any {
+        let value = this.languageConfiguration[`createTestFile.${key}`];
+        if (value !== undefined) {
+            return value;
+        }
+        return this.generalConfiguration.get(key, defaultValue);
+    }
 }
